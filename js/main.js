68b046c6978ac94850b7cc0f2b7b2957af19f078
Srinivas <kallurisrinu.web@gmail.com>
2024-03-18 17:17:36 +0530

    done

--- js/main.js
+++ js/main.js
@@ -32,3 +32,30 @@ $(".nav li").hover(function() {
 });
 
 ///  DROPDOWN MENU   ////
+
+ // Show the first tab by default
+ $('.tabs-stage div').hide();
+ $('.tabs-stage div div').show();
+ $('.tabs-stage div:first').show();
+ $('.tabs-nav li:first').addClass('tab-active');
+
+ // Change tab class and display content
+ $('.tabs-nav a').on('click', function (event) {
+   event.preventDefault();
+   $('.tabs-nav li').removeClass('tab-active');
+   $(this).parent().addClass('tab-active');
+   $('.tabs-stage div').hide();
+   $('.tabs-stage div div').show();
+   $($(this).attr('href')).show();
+ });
+ $('.loadbtn').on('click', function () {
+  $('.morecards').addClass('active');
+  $('.loadmore').addClass('active');
+  $(this).removeClass('active');
+});
+
+$('.loadbtn1').on('click', function () {
+  $('.morecards1').addClass('active');
+  $('.loadmore1').addClass('active');
+  $(this).removeClass('active');
+});
